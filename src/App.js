3f5d974a30dcf6b8e08958ba42f805ae8ebd269e
Mohammed-Ryaan <89339270+Mohammed-Ryaan@users.noreply.github.com>
2023-08-06 00:14:25 +0530

    Add files via upload
    New files added

--- src/App.js
+++ src/App.js
@@ -1,111 +1,115 @@
-import React, { useState } from 'react';
-import "./App.css";
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+
+// Components
+import Navigation from "./components/Navigation";
+import Search from "./components/Search";
+import Home from "./components/Home";
+
+// ABIs
+import RealEstate from "./abis/RealEstate.json";
+import Escrow from "./abis/Escrow.json";
+
+// Config
+import config from "./config.json";
 
 function App() {
-  const [data, setData] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [escrow, setEscrow] = useState(null);
 
-  const [users, setUsers] = useState([]);
-  const [name, setName] = useState("");
-  const [idType, setIdType] = useState("");
-  const [idNumber, setIdNumber] = useState("");
-  const [contactInfo, setContactInfo] = useState("");
+  const [account, setAccount] = useState(null);
 
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:8000/');
-        const jsonData = await response.json();
-        setData(jsonData);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+  const [homes, setHomes] = useState([]);
+  const [home, setHome] = useState({});
+  const [toggle, setToggle] = useState(false);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const newUser = {
-      name,
-      idType,
-      idNumber,
-      contactInfo,
-    };
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+    const network = await provider.getNetwork();
 
-    setUsers([...users, newUser]);
-    setName("");
-    setIdType("");
-    setIdNumber("");
-    setContactInfo("");
-  
-    fetchData();
+    const realEstate = new ethers.Contract(
+      config[network.chainId].realEstate.address,
+      RealEstate,
+      provider
+    );
+    const totalSupply = await realEstate.totalSupply();
+    const homes = [];
+
+    for (var i = 1; i <= totalSupply; i++) {
+      const uri = await realEstate.tokenURI(i);
+      const response = await fetch(uri);
+      const metadata = await response.json();
+      homes.push(metadata);
+    }
+
+    setHomes(homes);
+
+    const escrow = new ethers.Contract(
+      config[network.chainId].escrow.address,
+      Escrow,
+      provider
+    );
+    setEscrow(escrow);
+
+    window.ethereum.on("accountsChanged", async () => {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const account = ethers.utils.getAddress(accounts[0]);
+      setAccount(account);
+    });
+  };
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
+
+  const togglePop = (home) => {
+    setHome(home);
+    toggle ? setToggle(false) : setToggle(true);
   };
 
   return (
-    <>
     <div>
-      <h1>User Information</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          ID Type:
-          <input
-            type="text"
-            value={idType}
-            onChange={(e) => setIdType(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          ID Number:
-          <input
-            type="text"
-            value={idNumber}
-            onChange={(e) => setIdNumber(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Contact Info:
-          <input
-            type="text"
-            value={contactInfo}
-            onChange={(e) => setContactInfo(e.target.value)}
-          />
-        </label>
-        <br />
-        <button type="submit">Submit</button>
-      </form>
-      <h2>User List:</h2>
-      {users.length > 0 ? (
-        <ul>
-          {users.map((user, index) => (
-            <li key={index}>
-              <strong>Name:</strong> {user.name}, <strong>ID Type:</strong>{" "}
-              {user.idType}, <strong>ID Number:</strong> {user.idNumber},{" "}
-              <strong>Contact Info:</strong> {user.contactInfo}
-            </li>
+      <Navigation account={account} setAccount={setAccount} />
+      <Search />
+
+      <div className="cards__section">
+        <h3>Homes For You</h3>
+
+        <hr />
+
+        <div className="cards">
+          {homes.map((home, index) => (
+            <div className="card" key={index} onClick={() => togglePop(home)}>
+              <div className="card__image">
+                <img src={home.image} alt="Home" />
+              </div>
+              <div className="card__info">
+                <h4>{home.attributes[0].value} ETH</h4>
+                <p>
+                  <strong>{home.attributes[2].value}</strong> bds |
+                  <strong>{home.attributes[3].value}</strong> ba |
+                  <strong>{home.attributes[4].value}</strong> sqft
+                </p>
+                <p>{home.address}</p>
+              </div>
+            </div>
           ))}
-        </ul>
-      ) : (
-        <p>No users to display.</p>
-      )}
-      <div>
-        <h1>Data Fetching Example</h1>
-        {data ? (
-          <pre>{JSON.stringify(data, null, 2)}</pre>
-        ) : (
-          <p>No data fetched yet.</p>
-        )}
+        </div>
       </div>
+
+      {toggle && (
+        <Home
+          home={home}
+          provider={provider}
+          account={account}
+          escrow={escrow}
+          togglePop={togglePop}
+        />
+      )}
     </div>
-    </>
-   
   );
 }
 
