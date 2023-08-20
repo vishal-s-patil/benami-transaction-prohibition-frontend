13c1b83736b103700f51b553395ba17ab9e3da73
Mohammed-Ryaan <89339270+Mohammed-Ryaan@users.noreply.github.com>
2023-08-21 00:04:14 +0530

    Add files via upload

---
+++ client/src/index.js
@@ -0,0 +1,54 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Profile from "./components/Profile";
+import PropertyCard from "./components/PropertyCard";
+import Home from "./components/Home";
+import PropertyDetails from "./components/PropertyDetails";
+import store from "./utils/store";
+import { Provider } from "react-redux";
+import AddProperty from "./components/AddProperty";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+      {
+        path: "property/:id",
+        element: <PropertyDetails />,
+      },
+      {
+        path: "add-property",
+        element: <AddProperty />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={appRouter}>
+        <App />
+      </RouterProvider>
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
