a862a0ed85e32021a6dec7d231c2e40246145fc2
Mohammed-Ryaan <89339270+Mohammed-Ryaan@users.noreply.github.com>
2023-08-21 00:11:12 +0530

    Updated

---
+++ server/controllers/property_controller.js
@@ -0,0 +1,130 @@
+const propertyList = [
+  {
+    id: 2,
+    price: 6700000,
+    emi: 38400,
+    sqft: 684,
+    type: "1bhk",
+    image:
+      "https://images.nobroker.in/images/8a9fb6827e7bd179017e7bf504620f34/8a9fb6827e7bd179017e7bf504620f34_65142_861180_medium.jpg",
+    address: "Near DSR Diseria, Gunjur village, Bangalore",
+    owner_id: "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199",
+    isSale: true,
+  },
+  {
+    id: 3,
+    price: 5200000,
+    emi: 29800,
+    sqft: 570,
+    type: "2bhk",
+    image:
+      "https://images.nobroker.in/images/8a9fdf84899f6d330189a072ce5c2a05/8a9fdf84899f6d330189a072ce5c2a05_27718_medium.jpg",
+    address: "Rajaji Nagar, Bangalore",
+    owner_id: "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199",
+    isSale: true,
+  },
+  {
+    id: 5,
+    price: 9500000,
+    emi: 54200,
+    sqft: 1000,
+    type: "3bhk",
+    image:
+      "https://images.nobroker.in/images/8a9f927a7329c72f01732e55d3902594/8a9f927a7329c72f01732e55d3902594_67658_77021_medium.jpg",
+    address: "Bannerghatta Road, Bangalore",
+    owner_id: "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199",
+    isSale: true,
+  },
+  {
+    id: 7,
+    price: 3500000,
+    emi: 20000,
+    sqft: 450,
+    type: "1bhk",
+    image:
+      "https://images.nobroker.in/images/8a9f927a7329c72f01732e55d3902594/8a9f927a7329c72f01732e55d3902594_56412_343505_large.jpg",
+    address: "Electronic City Phase 1, Bangalore",
+    owner_id: "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199",
+    isSale: true,
+  },
+  {
+    id: 11,
+    price: 7800000,
+    emi: 44500,
+    sqft: 800,
+    type: "2bhk",
+    image:
+      "https://assets.nobroker.in/media/building/8a9f8f8379b8e0fe0179b95505c14695/images/8a9f8f8379b8e0fe0179b95505c14695_project_image_OhxPPF85bS1635602104517_82990_iris_original.jpg",
+    address: "Marathahalli, Bangalore",
+    owner_id: "0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc",
+    isSale: true,
+  },
+  {
+    id: 13,
+    price: 4200000,
+    emi: 24000,
+    sqft: 600,
+    type: "1bhk",
+    image:
+      "https://images.nobroker.in/images/8a9f927a7329c72f01732e55d3902594/8a9f927a7329c72f01732e55d3902594_18345_153807_large.jpg",
+    address: "Whitefield, Bangalore",
+    owner_id: "0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc",
+    isSale: true,
+  },
+  {
+    id: 17,
+    price: 6200000,
+    emi: 35500,
+    sqft: 750,
+    type: "2bhk",
+    image:
+      "https://images.nobroker.in/images/8a9f927a7329c72f01732e55d3902594/8a9f927a7329c72f01732e55d3902594_313_762565_medium.jpg",
+    address: "JP Nagar, Bangalore",
+    owner_id: "0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc",
+    isSale: true,
+  },
+  {
+    id: 19,
+    price: 10500000,
+    emi: 60000,
+    sqft: 1200,
+    type: "3bhk",
+    image:
+      "https://assets.nobroker.in/media/building/8a9f8f8379b8e0fe0179b95505c14695/images/8a9f8f8379b8e0fe0179b95505c14695_others_ga9d5Yz0pB1675754324675_21296_iris_original.jpg",
+    address: "HSR Layout, Bangalore",
+    owner_id: "0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc",
+    isSale: true,
+  },
+  {
+    id: 23,
+    price: 5800000,
+    emi: 33200,
+    sqft: 700,
+    type: "2bhk",
+    image:
+      "https://assets.nobroker.in/media/building/8a9fae827c5eb723017c5ee4d33b2977/images/8a9fae827c5eb723017c5ee4d33b2977_project_image_8TlRfMXDcF1681467563400_65477_iris_original.jpg",
+    address: "Koramangala, Bangalore",
+    owner_id: "0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc",
+    isSale: true,
+  },
+  {
+    id: 29,
+    price: 3800000,
+    emi: 21700,
+    sqft: 550,
+    type: "1bhk",
+    image:
+      "https://assets.nobroker.in/media/building/8a9fac827d47bedb017d47c8f8ef03f3/images/8a9fac827d47bedb017d47c8f8ef03f3_project_image_A1Mwd0PeBo1637643991770_94739_iris_original.jpg",
+    address: "Banashankari, Bangalore",
+    owner_id: "0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc",
+    isSale: true,
+  },
+];
+
+const getProperties = async (req, res) => {
+  res.status(200).json(propertyList);
+};
+
+module.exports = {
+  getProperties,
+};
