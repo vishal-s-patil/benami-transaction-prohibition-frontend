9b5457d761fb362d3dcc497b81b52545d37948ce
Mohammed-Ryaan <89339270+Mohammed-Ryaan@users.noreply.github.com>
2023-11-05 22:50:56 +0530

    Update userSlice.js

--- client/src/utils/userSlice.js
+++ client/src/utils/userSlice.js
@@ -5,6 +5,10 @@ const userSlice = createSlice({
   initialState: {
     userData: undefined,
     walletMoney: 0,
+    userRequests: undefined,
+    lendersList: undefined,
+    particularUserRequest: undefined,
+    particualarLenderList: undefined,
   },
   reducers: {
     setUser: (state, action) => {
@@ -14,8 +18,38 @@ const userSlice = createSlice({
     setWalletMoney: (state, action) => {
       state.walletMoney = action.payload;
     },
+    setUserRequests: (state, action) => {
+      state.userRequests = action.payload;
+    },
+    setLendersList: (state, action) => {
+      state.lendersList = action.payload;
+    },
+    getParticularUserRequest: (state, action) => {
+      const id = action.payload;
+      const particularUserRequest = state.userRequests.filter((user) => {
+        return user?.metamaskId == id;
+      });
+      console.log(state.userRequests);
+      console.log(particularUserRequest);
+      state.particularUserRequest = particularUserRequest?.data;
+    },
+    getParticularLendersList: (state, action) => {
+      const id = action.payload;
+      const particularLenderList = state.lendersList.filter((user) => {
+        return user?.metamaskId == id;
+      });
+      console.log(particularLenderList?.data);
+      state.particularLenderList = particularLenderList?.data;
+    },
   },
 });
 
-export const { setUser, setWalletMoney } = userSlice.actions;
+export const {
+  setUser,
+  setWalletMoney,
+  setUserRequests,
+  setLendersList,
+  getParticularLendersList,
+  getParticularUserRequest,
+} = userSlice.actions;
 export default userSlice.reducer;
